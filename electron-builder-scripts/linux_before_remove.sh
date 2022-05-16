2d375c23607ec8a384783bc9bad2db2989e4fd58
Hwiyeon Cho <hwiyeon.cho@makinarocks.ai>
2022-05-16 11:59:21 +0900

    :penguin: Update electron builder scripts for Linux

--- electron-builder-scripts/linux_before_remove.sh
+++ electron-builder-scripts/linux_before_remove.sh
@@ -1,1 +1,8 @@
-rm -rf "/opt/JupyterLab/resources/jlab_server"
+#!/bin/bash
+
+declare app_prefix="/opt/JupyterLab"
+declare env_installer="${app_prefix}/resources/env_installer"
+declare conda_prefix="${app_prefix}/resources/jlab_server"
+
+rm -rf "${conda_prefix}"
+rm -fr "${env_installer}"
